0eeb24b594e83f45dde1c4cf617bb3f59957dd01
akzhol <a.kanibekuly@gmail.com>
2022-04-13 11:27:55 +0600

    find email

--- internal/domain/utils/findEmail.go
+++ internal/domain/utils/findEmail.go
@@ -5,15 +5,19 @@ import (
 	"regexp"
 )
 
-var emailRegex = regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
-var pattern = regexp.MustCompile(`Email:\\c+`)
+var emailRegex = regexp.MustCompile(`[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+var pattern = regexp.MustCompile(`Email:\s+[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}`)
 
 func FindEmails(s string) []string {
-	//var res []string
-	fmt.Println(s)
-	data := pattern.Find([]byte(s))
-	fmt.Println(string(data), len(data))
-	return nil
+	var res []string
+	data := pattern.FindAllString(s, -1)
+	fmt.Println(data)
+	for i := range data {
+		fmt.Println(emailRegex.FindAllString(data[i], -1))
+		res = append(res, emailRegex.FindAllString(data[i], -1)...)
+	}
+
+	return res
 }
 
 func isEmailValid(e string) bool {
---
+++ internal/domain/utils/findEmail_test.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIsEmails(t *testing.T) {
+	cases := []struct {
+		s     string
+		valid bool
+	}{
+		{"a.kanibekuly@gmail.com", true},
+		{"test@gmail.com", true},
+	}
+
+	for i, c := range cases {
+		require.Equal(t, isEmailValid(c.s), c.valid, i+1)
+	}
+}
+
+func TestFindEmails(t *testing.T) {
+	cases := []struct {
+		in  string
+		out []string
+	}{
+		{
+			in:  "Email:  \na.kanibekuly@gmail.com",
+			out: []string{"a.kanibekuly@gmail.com"},
+		},
+		{
+			in:  "Email:  \na.kanibekuly@gmail.com asdvasv    Email:    \ttest@gmail.com",
+			out: []string{"a.kanibekuly@gmail.com", "test@gmail.com"},
+		},
+	}
+
+	for i, c := range cases {
+		require.Equal(t, FindEmails(c.in), c.out, i+1)
+	}
+}
